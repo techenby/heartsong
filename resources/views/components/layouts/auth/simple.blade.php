f17934634ff8d7c0232339d69dea0f770bd4c90e
Andy Newhouse <andy@techenby.com>
2025-10-21 09:45:48 -0500

    Add heartsong badge icon

--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -6,12 +6,12 @@
     <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
         <div class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div class="flex w-full max-w-sm flex-col gap-2">
-                <a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
-                    <span class="flex h-9 w-9 mb-1 items-center justify-center rounded-md">
-                        <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />
+                <div class="flex flex-col items-center gap-2 font-medium">
+                    <span class="flex size-24 mb-1 items-center justify-center rounded-md">
+                        <x-app-logo-icon class="size-24 fill-current text-black dark:text-white" />
                     </span>
-                    <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
-                </a>
+                    <span class="sr-only">{{ config('app.name', 'Heartsong') }}</span>
+                </div>
                 <div class="flex flex-col gap-6">
                     {{ $slot }}
                 </div>
