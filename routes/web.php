4094152a2610e1d5a7e08944b735edd8b31ef18b
Andy Newhouse <andy@techenby.com>
2025-10-21 10:57:08 -0500

    Add custom favicon

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,7 @@ use App\Livewire\Settings\TwoFactor;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::redirect('/', '/login')->name('home');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
