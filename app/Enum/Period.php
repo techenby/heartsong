1f6c0713f58de15381dff2076d4d2b7ae0fb2174
Andy Newhouse <andy@techenby.com>
2025-10-22 07:11:57 -0500

    Fix timing

--- app/Enum/Period.php
+++ app/Enum/Period.php
@@ -14,14 +14,14 @@ enum Period: string
 
     public function times(): array
     {
-        return match($this) {
-            self::FIRST => ['start' => '08:30:00', 'end' => '09:30:00'],
-            self::SECOND => ['start' => '09:35:00', 'end' => '10:35:00'],
-            self::THIRD => ['start' => '10:40:00', 'end' => '11:40:00'],
-            self::FOURTH => ['start' => '11:40:00', 'end' => '12:25:00'],
-            self::FIFTH => ['start' => '12:25:00', 'end' => '13:25:00'],
-            self::SIXTH => ['start' => '13:30:00', 'end' => '15:30:00'],
-            self::SEVENTH => ['start' => '15:35:00', 'end' => '16:15:00'],
+        return match ($this) {
+            self::FIRST => ['start' => '08:20:00', 'end' => '09:20:00'],
+            self::SECOND => ['start' => '09:20:00', 'end' => '10:20:00'],
+            self::THIRD => ['start' => '10:20:00', 'end' => '11:20:00'],
+            self::FOURTH => ['start' => '11:20:00', 'end' => '12:10:00'],
+            self::FIFTH => ['start' => '12:10:00', 'end' => '13:10:00'],
+            self::SIXTH => ['start' => '13:15:00', 'end' => '15:15:00'],
+            self::SEVENTH => ['start' => '15:15:00', 'end' => '16:15:00'],
         };
     }
 }
