e302840917f7ded25890b63fde4741c09eea5099
Andy Newhouse <andy@techenby.com>
2025-11-04 09:47:42 -0600

    Dusting 🧹

--- tests/Unit/Jobs/ImportRosterTest.php
+++ tests/Unit/Jobs/ImportRosterTest.php
@@ -4,7 +4,6 @@ use App\Jobs\ImportRoster;
 use App\Models\Course;
 use App\Models\Student;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
@@ -15,7 +14,7 @@ it('imports students from a CSV, replaces existing students, trims names, and de
 
     Student::factory()->for($course)->count(2)->createMany([
         ['first_name' => 'Existing', 'last_name' => 'Student'],
-        ['first_name' => 'To', 'last_name' => 'Delete']
+        ['first_name' => 'To', 'last_name' => 'Delete'],
     ]);
 
     $absolutePath = storage_path('app/private/imports/roster.csv');
@@ -32,8 +31,8 @@ it('imports students from a CSV, replaces existing students, trims names, and de
 
     expect($course->students)->toHaveCount(2)
         ->and($course->students)->sequence(
-            fn($studentA) => $studentA->first_name->toEqual('Jane') && $studentA->last_name->toEqual('Doe'),
-            fn($studentB) => $studentB->first_name->toEqual('John') && $studentB->last_name->toEqual('Smith'),
+            fn ($studentA) => $studentA->first_name->toEqual('Jane') && $studentA->last_name->toEqual('Doe'),
+            fn ($studentB) => $studentB->first_name->toEqual('John') && $studentB->last_name->toEqual('Smith'),
         );
 
     Storage::shouldHaveReceived('delete')->once()->with($absolutePath);
